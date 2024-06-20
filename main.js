e95360a854282f60f3d60591c53368ccf6b9c9e8
NoTUsR <34623800+NoTUsR@users.noreply.github.com>
2024-06-21 02:34:45 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 import * as THREE from 'three';
 
-console.log(THREE)
+const container = document.querySelector('#teddy_container')
+let width = container.getBoundingClientRect().width
+let height = container.getBoundingClientRect().height
+const canvas = document.querySelector('#teddy_canvas');
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 45, width / height, 0.1, 1000 );
+camera.position.set(0, 1, 3)
+console.log(container, canvas)
